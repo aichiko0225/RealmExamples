f270e206e50d016ad85566267ff881f84add25f4
ash <aichiko66@163.com>
2019-04-10 08:44:48 +0800

    remove Pods

--- Pods/Target Support Files/cocore/cocore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_cocore : NSObject
-@end
-@implementation PodsDummy_cocore
-@end
--- Pods/Target Support Files/coobjc/coobjc-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_coobjc : NSObject
-@end
-@implementation PodsDummy_coobjc
-@end
